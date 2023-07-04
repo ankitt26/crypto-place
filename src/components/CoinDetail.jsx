ffcf5f8a0a276bca07a0ca13751522b1008a4c6d
Ankit <ankit26kushwa@gmail.comm>
2023-07-04 21:01:44 +0530

    Create CoinDetail page

---
+++ src/components/CoinDetail.jsx
@@ -0,0 +1,45 @@
+const CoinDetail = () => {
+  return (
+    <main>
+      <div>
+        <button> « </button>
+        <h2>Bitcoin</h2>
+      </div>
+      <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"></img>
+      <div>
+        <div>
+          <h3>current_price </h3>
+          <h3> market_cap</h3>
+          <h3>total_volume</h3>
+          <h3>high_24h</h3>
+          <h3>low_24h</h3>
+          <h3>price_change_24h</h3>
+          <h3>price_change_percentage_24h</h3>
+          <h3>market_cap_change_24h</h3>
+          <h3>market_cap_change_percentage_24h</h3>
+          <h3>price_change_percentage_1h_in_currency</h3>
+          <h3> price_change_percentage_24h_in_currency</h3>
+          <h3>price_change_percentage_7d_in_currency</h3>
+          <h3>last_updated</h3>
+        </div>
+        <div>
+          <h3>current_price </h3>
+          <h3> market_cap</h3>
+          <h3>total_volume</h3>
+          <h3>high_24h</h3>
+          <h3>low_24h</h3>
+          <h3>price_change_24h</h3>
+          <h3>price_change_percentage_24h</h3>
+          <h3>market_cap_change_24h</h3>
+          <h3>market_cap_change_percentage_24h</h3>
+          <h3>price_change_percentage_1h_in_currency</h3>
+          <h3> price_change_percentage_24h_in_currency</h3>
+          <h3>price_change_percentage_7d_in_currency</h3>
+          <h3>last_updated</h3>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default CoinDetail;
