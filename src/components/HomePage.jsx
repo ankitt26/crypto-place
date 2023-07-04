36dcc45ef38aa1e9e403495d60e3cee8c8ea5d05
Ankit <ankit26kushwa@gmail.comm>
2023-07-04 20:42:14 +0530

    Add styling in Home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,20 +4,163 @@ const HomePage = () => {
   return (
     <>
       <Navbar />
-      <main>
-        <div className={styles.card1}>
-          <img className={styles.img1} src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"></img>
+      <main className={styles.main}>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+          ></img>
           <div className={styles.detail_box}>
             <div className={styles.box1}>
-              <h3 className={styles.name}>Coin</h3>
-              <h4 className={styles.symbol}>Symbol</h4>
-              <h4 className={styles.price}>Current Price</h4>
+              <h4 className={styles.name}>Coin</h4>
+              <h5 className={styles.symbol}>Symbol</h5>
+              <h5 className={styles.price}>Current Price</h5>
               <h5 className={styles.change}>24h Change</h5>
             </div>
             <div className={styles.box2}>
-              <h3 className={styles.name}>Bitcoin</h3>
-              <h4 className={styles.symbol}>btc</h4>
-              <h4 className={styles.price}>$31.1k</h4>
+              <h4 className={styles.name}>Bitcoin</h4>
+              <h5 className={styles.symbol}>btc</h5>
+              <h5 className={styles.price}>$31.1k</h5>
+              <h5 className={styles.change}>1.3%</h5>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+          ></img>
+          <div className={styles.detail_box}>
+            <div className={styles.box1}>
+              <h4 className={styles.name}>Coin</h4>
+              <h5 className={styles.symbol}>Symbol</h5>
+              <h5 className={styles.price}>Current Price</h5>
+              <h5 className={styles.change}>24h Change</h5>
+            </div>
+            <div className={styles.box2}>
+              <h4 className={styles.name}>Bitcoin</h4>
+              <h5 className={styles.symbol}>btc</h5>
+              <h5 className={styles.price}>$31.1k</h5>
+              <h5 className={styles.change}>1.3%</h5>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+          ></img>
+          <div className={styles.detail_box}>
+            <div className={styles.box1}>
+              <h4 className={styles.name}>Coin</h4>
+              <h5 className={styles.symbol}>Symbol</h5>
+              <h5 className={styles.price}>Current Price</h5>
+              <h5 className={styles.change}>24h Change</h5>
+            </div>
+            <div className={styles.box2}>
+              <h4 className={styles.name}>Bitcoin</h4>
+              <h5 className={styles.symbol}>btc</h5>
+              <h5 className={styles.price}>$31.1k</h5>
+              <h5 className={styles.change}>1.3%</h5>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+          ></img>
+          <div className={styles.detail_box}>
+            <div className={styles.box1}>
+              <h4 className={styles.name}>Coin</h4>
+              <h5 className={styles.symbol}>Symbol</h5>
+              <h5 className={styles.price}>Current Price</h5>
+              <h5 className={styles.change}>24h Change</h5>
+            </div>
+            <div className={styles.box2}>
+              <h4 className={styles.name}>Bitcoin</h4>
+              <h5 className={styles.symbol}>btc</h5>
+              <h5 className={styles.price}>$31.1k</h5>
+              <h5 className={styles.change}>1.3%</h5>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+          ></img>
+          <div className={styles.detail_box}>
+            <div className={styles.box1}>
+              <h4 className={styles.name}>Coin</h4>
+              <h5 className={styles.symbol}>Symbol</h5>
+              <h5 className={styles.price}>Current Price</h5>
+              <h5 className={styles.change}>24h Change</h5>
+            </div>
+            <div className={styles.box2}>
+              <h4 className={styles.name}>Bitcoin</h4>
+              <h5 className={styles.symbol}>btc</h5>
+              <h5 className={styles.price}>$31.1k</h5>
+              <h5 className={styles.change}>1.3%</h5>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+          ></img>
+          <div className={styles.detail_box}>
+            <div className={styles.box1}>
+              <h4 className={styles.name}>Coin</h4>
+              <h5 className={styles.symbol}>Symbol</h5>
+              <h5 className={styles.price}>Current Price</h5>
+              <h5 className={styles.change}>24h Change</h5>
+            </div>
+            <div className={styles.box2}>
+              <h4 className={styles.name}>Bitcoin</h4>
+              <h5 className={styles.symbol}>btc</h5>
+              <h5 className={styles.price}>$31.1k</h5>
+              <h5 className={styles.change}>1.3%</h5>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+          ></img>
+          <div className={styles.detail_box}>
+            <div className={styles.box1}>
+              <h4 className={styles.name}>Coin</h4>
+              <h5 className={styles.symbol}>Symbol</h5>
+              <h5 className={styles.price}>Current Price</h5>
+              <h5 className={styles.change}>24h Change</h5>
+            </div>
+            <div className={styles.box2}>
+              <h4 className={styles.name}>Bitcoin</h4>
+              <h5 className={styles.symbol}>btc</h5>
+              <h5 className={styles.price}>$31.1k</h5>
+              <h5 className={styles.change}>1.3%</h5>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img
+            className={styles.img}
+            src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+          ></img>
+          <div className={styles.detail_box}>
+            <div className={styles.box1}>
+              <h4 className={styles.name}>Coin</h4>
+              <h5 className={styles.symbol}>Symbol</h5>
+              <h5 className={styles.price}>Current Price</h5>
+              <h5 className={styles.change}>24h Change</h5>
+            </div>
+            <div className={styles.box2}>
+              <h4 className={styles.name}>Bitcoin</h4>
+              <h5 className={styles.symbol}>btc</h5>
+              <h5 className={styles.price}>$31.1k</h5>
               <h5 className={styles.change}>1.3%</h5>
             </div>
           </div>
