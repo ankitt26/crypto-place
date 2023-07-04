e9daacd4a6df20032be1f5dc4c9dfac6d1810ebc
Ankit <ankit26kushwa@gmail.comm>
2023-07-04 21:51:20 +0530

    Add routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 import HomePage from "./components/HomePage.jsx";
+import CoinDetail from "./components/CoinDetail";
 const App = () => {
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/coin_detail" element={<CoinDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
