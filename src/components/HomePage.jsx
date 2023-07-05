b2d802d086fe16c32dc11128e532165033e51306
Ankit <ankit26kushwa@gmail.comm>
2023-07-05 16:28:30 +0530

    Add search functionality

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,52 +4,64 @@ import styles from "../CSS/HomePage.module.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FetchData } from "../redux/Coins/CoinSlice";
-const HomePage = () => {
-  let coin_id = "bitcoin";
 
+
+const HomePage = () => {
   const dispatch = useDispatch();
-  const data = useSelector((state)=>state.Coins);
+  const data = useSelector((state) => state.Coins);
+  const SearchKey = useSelector((state) => state.Search.searchkey);
+  console.log(SearchKey);
   const { CoinList, Loading, Error } = data;
-  // console.log(CoinList);
-  useEffect(() => {
-    if(CoinList.length === 0){
-        dispatch(FetchData());
+
+  let finalData = CoinList;
+
+  if (SearchKey !== "") {
+    finalData = CoinList.filter((element) => {
+      return element.id.includes(SearchKey) || element.name.includes(SearchKey);
+    });
+
+    if (finalData.length === 0) {
+      console.log("no matched");
     }
-    // console.log('fetchdata');
+  }
+
+ 
+
+  useEffect(() => {
+    if (CoinList.length === 0) {
+      dispatch(FetchData());
+    }
   }, [dispatch]);
 
-  const renderCoins = CoinList.map(element => {
-    // console.log(element.id);
-    return(
+  const renderCoins = finalData.map((element) => {
+    return (
       <Link className={styles.card} to={`/coin/${element.id}`} key={element.id}>
-      <img
-        className={styles.img}
-        src={element.image}
-      ></img>
-      <div className={styles.detail_box}>
-        <div className={styles.box1}>
-          <h4 className={styles.name}>Coin</h4>
-          <h5 className={styles.symbol}>Symbol</h5>
-          <h5 className={styles.price}>Current Price</h5>
-          <h5 className={styles.change}>24h Change</h5>
+        <img className={styles.img} src={element.image}></img>
+        <div className={styles.detail_box}>
+          <div className={styles.box1}>
+            <h4 className={styles.name}>Coin</h4>
+            <h5 className={styles.symbol}>Symbol</h5>
+            <h5 className={styles.price}>Current Price</h5>
+            <h5 className={styles.change}>24h Change</h5>
+          </div>
+          <div className={styles.box2}>
+            <h4 className={styles.name}>{element.name}</h4>
+            <h5 className={styles.symbol}>{element.symbol}</h5>
+            <h5 className={styles.price}>{element.current_price}</h5>
+            <h5 className={styles.change}>
+              {element.price_change_percentage_24h}
+            </h5>
+          </div>
         </div>
-        <div className={styles.box2}>
-          <h4 className={styles.name}>{element.name}</h4>
-          <h5 className={styles.symbol}>{element.symbol}</h5>
-          <h5 className={styles.price}>{element.current_price}</h5>
-          <h5 className={styles.change}>{element.price_change_percentage_24h}</h5>
-        </div>
-      </div>
-    </Link>
-    )
-  })
+      </Link>
+    );
+  });
 
   return (
     <>
       <Navbar />
-      <main className={styles.main}>
-       {renderCoins}
-      </main>
+     
+      <main className={styles.main}>{renderCoins}</main>
     </>
   );
 };
